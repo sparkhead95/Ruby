5486d3121c899c611b21837a51dc95f74623d555
sparkhead95 <christianosholland@gmail.com>
2016-11-30 03:47:52 +0000

    Still nothing happened... seem to have got rid of the app is not defined error

--- csa/app/channels/application_cable/channel.rb
+++ csa/app/channels/application_cable/channel.rb
@@ -1,17 +1,4 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
-    
-  def subscribed
-    stream_from 'broadcast_channel'
-  end  
-
-  def unsubscribed
-
-  end
-
-  def speak
-    ActionCable.server.broadcast "application_channel", message: data['message']
-  end
-
   end
 end
